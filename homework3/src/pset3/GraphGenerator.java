35df7d348b9e17fc275142d8a63729767b986584
Akaash Chikarmane <avchikar97@gmail.com>
2019-03-17 20:16:55 -0500

    initial commit for createCFGWithMethodInvocation(), same as createCFG() but with INVOKESTATIC

--- homework3/src/pset3/GraphGenerator.java
+++ homework3/src/pset3/GraphGenerator.java
@@ -44,6 +44,52 @@ public class GraphGenerator {
     }
     public CFG createCFGWithMethodInvocation(String className) throws ClassNotFoundException {
         // your code goes here
+        CFG cfg = new CFG();
+        JavaClass jc = Repository.lookupClass(className);
+        ClassGen cg = new ClassGen(jc);
+        ConstantPoolGen cpg = cg.getConstantPool();
+        Method[] methods = cg.getMethods();
+
+        for (Method m: cg.getMethods()) {
+            MethodGen mg = new MethodGen(m, cg.getClassName(), cpg);
+            InstructionList il = mg.getInstructionList();
+            InstructionHandle[] handles = il.getInstructionHandles();
+            for (InstructionHandle ih : handles) {
+                int position = ih.getPosition();
+                cfg.addNode(position, m, jc);
+                Instruction inst = ih.getInstruction();
+                InstructionHandle inst_hdl_nxt = ih.getNext();
+
+                // your code goes here
+                // need to add edges
+                // figure out if it's a branching instruction or not
+                if (inst instanceof BranchInstruction) {
+                    // if it is, figure out where it's going and make an edge
+                    BranchInstruction inst_br = (BranchInstruction) inst;
+                    InstructionHandle inst_hdl = inst_br.getTarget();
+                    int pos2 = inst_hdl.getPosition();
+                    cfg.addEdge(position, m, jc, pos2, m, jc);
+                }
+
+                if (inst instanceof INVOKESTATIC) {
+                    INVOKESTATIC inst_invk_stat = (INVOKESTATIC) inst;
+                    String str_inst = inst.toString();
+                    int invk_index = str_inst.lastIndexOf(' ') + 1;
+                    int method_index = Integer.parseInt(str_inst.substring(invk_index));
+                    if (method_index <= methods.length) {
+                        Method invoked_method = methods[method_index];
+                        cfg.addEdge(position, m, jc, 0, invoked_method, jc);
+                    }
+                }
+
+                // if it's not a branch instruction, make sure there's something next and it's not the final bytecode instruction for the method
+                if ((inst_hdl_nxt != null) && (!inst.toString().contains("return")) && (!inst.toString().contains("goto"))) {
+                    int pos2 = inst_hdl_nxt.getPosition();
+                    cfg.addEdge(position, m, jc, pos2, m, jc);
+                }
+            }
+        }
+        return cfg;
     }
 
     public static void main(String[] a) throws ClassNotFoundException {
